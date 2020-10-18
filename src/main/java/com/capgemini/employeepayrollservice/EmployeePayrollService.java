ab7d9dabb08f93b9abdd6a51b88144d4477fc150
mamudsamiul <samiulmamud1995@gmail.com>
2020-10-19 00:51:51 +0530

    (Samiul Mamud) Created: Employee Payroll service Program is created

---
+++ src/main/java/com/capgemini/employeepayrollservice/EmployeePayrollService.java
@@ -0,0 +1,8 @@
+package com.capgemini.employeepayrollservice;
+
+public class EmployeePayrollService {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the employee payroll service!");
+	}
+}
---
+++ src/test/java/com/capgemini/payrolltest/PayrollTest.java
@@ -0,0 +1,10 @@
+package com.capgemini.payrolltest;
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+public class PayrollTest {
+   
+}
